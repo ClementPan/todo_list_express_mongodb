0bbe3c0ddeeee6d847e697a6fd53235cc8f255c0
ClementPan <zzz2999582@gmail.com>
2020-07-27 10:33:48 +0800

    feat: minor arrangements

--- models/seeds/todoSeeder.js
+++ models/seeds/todoSeeder.js
@@ -8,6 +8,8 @@ const db = mongoose.connection
 db.on('error', () => {
   console.log('mongodb error!')
 })
+
+
 db.once('open', () => {
   console.log('mongodb connected!')
   for (let i = 0; i < 10; i++) {
